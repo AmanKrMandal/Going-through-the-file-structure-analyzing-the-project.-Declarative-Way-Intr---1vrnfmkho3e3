e4d0457d2db95cdcc18105fd7267e2588c2bcb0a
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-06-28 12:42:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">E-Commerce </h1>
+            <p>E-Commerce project management is a branch of traditional project management that has its own specifics. Basically, it is the practice of applying various skills, tools, techniques, and approaches as well as performing various activities aimed at the effective development of an e-commerce project.</p>
+    </div>
   )
 }
 
